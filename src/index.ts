31018b123eca9e801add5b9f7ce4151fb5fd3af4
Mishu <mishudebnath927@gmail.com>
2026-07-12 00:36:57 +0600

    create a all events get api

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI!;
 
 // App
 const app = express();
@@ -39,7 +39,15 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+ interface EventData {
+  title: string;
+  shortDescription: string;
+  fullDescription: string;
+  price: number;
+  date: string;
+  priority: string;
+  imageUrl: string;
+ }
 async function run() {
     const db = client.db('eventHub')
     const events = db.collection('events');
@@ -48,14 +56,14 @@ async function run() {
     // post one events
         app.post('/api/events',  async (req:Request, res:Response) => {
             try {
-                const eventsData = req.body;
+                const eventData:EventData = req.body;
 
-                const result = await events.insertOne(eventsData);
+                const result = await events.insertOne(eventData);
 
                 res.status(201).send({
                     success: true,
-                    message: 'events added successfully',
-                    data: result
+                    message: "Event added successfully",
+                     insertedId: result.insertedId,
                 });
                 console.log(result);
             } catch (error:any) {
@@ -67,6 +75,28 @@ async function run() {
                     message: 'Failed to add events',
                     error: error.message 
                 })
+            }
+        });
+
+
+        // get all events 
+        app.get('/api/events', async (req:Request, res:Response) => {
+            try {
+               
+                const eventsData = await events.find().toArray();
+
+                res.status(200).send({
+                    success: true,
+                    message: "Events fetched successfully",
+                    data: eventsData,
+                });
+
+            } catch (error:any) {
+                res.status(500).send({
+                    success: false,
+                    message: "Failed to fetch events",
+                    error: error.message,
+                });
             }
         });
 
