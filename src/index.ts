1d81f52e423ad6c38583cc1a5949551e68a2c510
Mishu <mishudebnath927@gmail.com>
2026-07-14 01:24:39 +0600

    fix: console.dir typescript compilation error

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@
  */
 
 import dns from "node:dns/promises";
-import express, { Request, Response,NextFunction } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
@@ -35,7 +35,11 @@ const client = new MongoClient(uri, {
   },
 });
 
-client.connect(() => console.log("connecting to mongo db")).catch(console.log.dir)
+// MongoDB Connection
+client
+  .connect()
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 interface EventData {
   title: string;
@@ -49,21 +53,16 @@ interface EventData {
   location: string;
 }
 
-// async function run() {
-
-  const db = client.db("eventHub");
-  const events = db.collection("events");
-  const sessions = db.collection('session');
-  const users = db.collection('user');
-
-
-
+const db = client.db("eventHub");
+const events = db.collection("events");
+const sessions = db.collection("session");
+const users = db.collection("user");
 
 interface CustomRequest extends Request {
   user?: any;
 }
 
- const verifyToken = async (
+const verifyToken = async (
   req: CustomRequest,
   res: Response,
   next: NextFunction
@@ -71,21 +70,17 @@ interface CustomRequest extends Request {
   const header = req.headers.authorization;
   if (!header) {
     return res.status(401).send({
-      message: "Unauthorized access", 
+      message: "Unauthorized access",
     });
   }
 
   const token = header.split(" ")[1];
-
-
 
   if (!token) {
     return res.status(401).send({
       message: "Unauthorized access",
     });
   }
-
-  
 
   const query = { token: token };
   const session = await sessions.findOne(query);
@@ -110,49 +105,37 @@ interface CustomRequest extends Request {
   }
 
   req.user = user;
-
   next();
 };
-  
 
-  // try {
-    // Connect the client to the server	(optional starting in v4.7)
-  //   await client.connect();
+// post one events
+app.post("/api/events", verifyToken, async (req: Request, res: Response) => {
+  try {
+    const eventData: EventData = req.body;
+    const result = await events.insertOne(eventData);
 
-    // post one events
-    app.post("/api/events",verifyToken, async (req: Request, res: Response) => {
-      try {
-        const eventData: EventData = req.body;
-
-        const result = await events.insertOne(eventData);
-
-        res.status(201).send({
-          success: true,
-          message: "Event added successfully",
-          insertedId: result.insertedId,
-        });
-        
-      } catch (error: any) {
-        console.log(error);
-
-        res.status(500).send({
-          success: false,
-          message: "Failed to add events",
-          error: error.message,
-        });
-      }
+    res.status(201).send({
+      success: true,
+      message: "Event added successfully",
+      insertedId: result.insertedId,
     });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).send({
+      success: false,
+      message: "Failed to add events",
+      error: error.message,
+    });
+  }
+});
 
-
-    // get all events
+// get all events
 app.get("/api/events", async (req: Request, res: Response) => {
   try {
     const { search, category, location, sortBy, page, itemsPerPage } = req.query;
 
-  
     let query: Record<string, any> = {};
 
-    
     if (search && typeof search === "string") {
       query.$or = [
         { title: { $regex: search, $options: "i" } },
@@ -161,7 +144,6 @@ app.get("/api/events", async (req: Request, res: Response) => {
       ];
     }
 
-    
     if (category && typeof category === "string" && category !== "All Categories") {
       query.category = {
         $regex: `^${category.trim()}$`,
@@ -169,8 +151,6 @@ app.get("/api/events", async (req: Request, res: Response) => {
       };
     }
 
-
-  
     if (location && typeof location === "string" && location !== "All Locations") {
       query.location = {
         $regex: `^${location.trim()}$`,
@@ -178,28 +158,24 @@ app.get("/api/events", async (req: Request, res: Response) => {
       };
     }
 
-   
-let sortOption: Record<string, any> = { _id: -1 }; 
+    let sortOption: Record<string, any> = { _id: -1 };
 
-if (sortBy === "Newest") {
-  sortOption = { date: -1 }; 
-} else if (sortBy === "Oldest") {
-  
-  sortOption = { date: 1 };  
-} else if (sortBy === "PriceLowToHigh") {
-  sortOption = { price: 1 }; 
-} else if (sortBy === "PriceHighToLow") {
-  sortOption = { price: -1 }; 
-}
+    if (sortBy === "Newest") {
+      sortOption = { date: -1 };
+    } else if (sortBy === "Oldest") {
+      sortOption = { date: 1 };
+    } else if (sortBy === "PriceLowToHigh") {
+      sortOption = { price: 1 };
+    } else if (sortBy === "PriceHighToLow") {
+      sortOption = { price: -1 };
+    }
 
-    
     const total = await events.countDocuments(query);
-    
+
     const currentPage = parseInt(page as string) || 1;
     const limit = parseInt(itemsPerPage as string) || 8;
     const skip = (currentPage - 1) * limit;
 
-  
     const result = await events
       .find(query)
       .sort(sortOption)
@@ -207,7 +183,6 @@ if (sortBy === "Newest") {
       .limit(limit)
       .toArray();
 
- 
     res.status(200).send({
       success: true,
       message: "Events fetched successfully",
@@ -225,78 +200,77 @@ if (sortBy === "Newest") {
   }
 });
 
+app.get("/api/events/featured", async (req: Request, res: Response) => {
+  try {
+    const eventsData = await events.find().limit(8).toArray();
 
-    app.get("/api/events/featured", async (req: Request, res: Response) => {
-      try {
-        const eventsData = await events.find().limit(8).toArray();
-
-        res.status(200).send({
-          success: true,
-          message: "Featured events fetched successfully",
-          data: eventsData,
-        });
-      } catch (error: any) {
-        res.status(500).send({
-          success: false,
-          message: "Failed to fetch featured events",
-          error: error.message,
-        });
-      }
+    res.status(200).send({
+      success: true,
+      message: "Featured events fetched successfully",
+      data: eventsData,
     });
-  
-    // get events by id
-    app.get("/api/events/:id", async (req: Request, res: Response) => {
-      try {
-        const { id } = req.params;
-        if (typeof id !== "string") {
-          return res.status(400).send({
-            success: false,
-            message: "Invalid event id",
-          });
-        }
-
-        if (!ObjectId.isValid(id)) {
-          return res.status(400).send({
-            success: false,
-            message: "Invalid ObjectId",
-          });
-        }
-
-        const query = {
-          _id: new ObjectId(id),
-        };
-
-        const event = await events.findOne(query);
-
-        if (!event) {
-          return res.status(404).send({
-            success: false,
-            message: "Event not found",
-          });
-        }
-
-        return res.status(200).send({
-          success: true,
-          message: "Event fetched successfully",
-          data: event,
-        });
-      } catch (error) {
-        if (error instanceof Error) {
-          return res.status(500).send({
-            success: false,
-            message: "Failed to fetch event",
-            error: error.message,
-          });
-        }
-
-        return res.status(500).send({
-          success: false,
-          message: "Unknown error occurred",
-        });
-      }
+  } catch (error: any) {
+    res.status(500).send({
+      success: false,
+      message: "Failed to fetch featured events",
+      error: error.message,
     });
+  }
+});
 
-    app.get("/api/events/user/:userId",verifyToken, async (req: Request, res: Response) => {
+// get events by id
+app.get("/api/events/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    if (typeof id !== "string") {
+      return res.status(400).send({
+        success: false,
+        message: "Invalid event id",
+      });
+    }
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).send({
+        success: false,
+        message: "Invalid ObjectId",
+      });
+    }
+
+    const query = {
+      _id: new ObjectId(id),
+    };
+
+    const event = await events.findOne(query);
+
+    if (!event) {
+      return res.status(404).send({
+        success: false,
+        message: "Event not found",
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: "Event fetched successfully",
+      data: event,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).send({
+        success: false,
+        message: "Failed to fetch event",
+        error: error.message,
+      });
+    }
+
+    return res.status(500).send({
+      success: false,
+      message: "Unknown error occurred",
+    });
+  }
+});
+
+app.get("/api/events/user/:userId", verifyToken, async (req: Request, res: Response) => {
   try {
     const { userId } = req.params;
 
@@ -334,9 +308,8 @@ if (sortBy === "Newest") {
   }
 });
 
-
 // delete event by id
-app.delete("/api/events/:id",verifyToken, async (req: Request, res: Response) => {
+app.delete("/api/events/:id", verifyToken, async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
 
@@ -388,27 +361,14 @@ app.delete("/api/events/:id",verifyToken, async (req: Request, res: Response) =>
   }
 });
 
-module.exports = app;
-
-
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!",
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
 // Routes
 app.get("/", (req: Request, res: Response) => {
   res.send("EventHub API");
 });
 
-// Server
+// Server Listen
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
